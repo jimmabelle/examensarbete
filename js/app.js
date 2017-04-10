b4cb3efdd6abb1daa06d924a4d2ad6571916ae3f
jimmabelle <jimmabelleblikstad01@gmail.com>
2017-04-10 16:26:37 +0200

    responsive style

--- js/app.js
+++ js/app.js
@@ -1,31 +1,3 @@
-/*
-// Get the modal & modal overlay
-var modal = document.getElementById('my-modal');
-
-// Get the button that opens the modal
-var btn = document.getElementById('reg-btn');
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('modal-close')[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = 'block';
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = 'none';
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-}
-*/
-
 // Get the modal & modal overlay
 var modalOne = document.getElementById('modal-one');
 var modalTwo = document.getElementById('modal-two');
@@ -38,7 +10,7 @@ var btnLog = document.getElementById('log-btn');
 var spanOne = document.getElementsByClassName('modal-close-one')[0];
 var spanTwo = document.getElementsByClassName('modal-close-two')[0];
 
-// When the user clicks on the button, open the modal
+// When the user clicks on the button, open the modal or close 
 function addListeners() {
   if (window.addEventListener) {
     btnReg.addEventListener("click", modalReg, false);
---
+++ js/menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $(".navi-menu").on("click", function() {
+    $("nav ul").toggleClass("open");
+  });
+});
